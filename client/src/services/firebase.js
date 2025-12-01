30b6f49a1ea82d08887cf876bbb2dc35a7a927c5
Antonio Marcos <marcosmacedo784@gmail.com>
2025-12-01 12:18:03 -0300

    Firs commit

--- client/src/services/firebase.js
+++ client/src/services/firebase.js
@@ -9,13 +9,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDGZFcyiAX2siRTgRCEWa13EaWtzvxJ0p0",
-  authDomain: "avalia-22c72.firebaseapp.com",
-  projectId: "avalia-22c72",
-  storageBucket: "avalia-22c72.firebasestorage.app",
-  messagingSenderId: "894950542554",
-  appId: "1:894950542554:web:4919b4b5ce2b20a48369e3",
-  measurementId: "G-E4H8KSES6N"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
